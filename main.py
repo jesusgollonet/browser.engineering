40e3b2b5ffe8dddaed754680f281e4cf8aa77b3e
Jesus Gollonet <jesusgollonet@gmail.com>
2024-07-22 17:24:10 +0200

    Implement request with initial html parsing

--- main.py
+++ main.py
@@ -1,14 +1,68 @@
+import socket
+
+
 class URL:
     def __init__(self, url):
         self.scheme, url = url.split("://", 1)
-        print(self.scheme, url)
         if "/" not in url:
             url += "/"
         self.host, url = url.split("/", 1)
         self.path = "/" + url
-        print(self.host, url)
         assert self.scheme == "http"
 
+    def request(self):
+        s = socket.socket(
+            family=socket.AF_INET, type=socket.SOCK_STREAM, proto=socket.IPPROTO_TCP
+        )
+        s.connect((self.host, 80))
+        request = f"GET {self.path} HTTP/1.0\r\n"
+        request += f"Host: {self.host}\r\n"
+        request += "\r\n"
+        s.send(request.encode("utf8"))
+        response = s.makefile("r", encoding="utf8", newline="\r\n")
+        statusline = response.readline()
+        version, status, explanation = statusline.split(" ", 2)
+        print(version, status, explanation)
+        response_headers = {}
+        while True:
+            line = response.readline()
+            print(line)
+            if line == "\r\n":
+                break
+            header, value = line.split(":", 1)
+            response_headers[header.casefold()] = value.strip()
+        print(response_headers)
+        # we won't handle these
+        assert "transfer-encoding" not in response_headers
+        assert "content-encoding" not in response_headers
+        body = response.read()
+        print(body)
+        s.close()
+        return body
 
-URL("http://www.google.com")
-URL("http://www.google.com/index.html")
+
+def load(url):
+    body = url.request()
+    show(body)
+
+
+def show(body):
+    print(body)
+    in_tag = False
+    for c in body:
+        if c == "<":
+            in_tag = True
+        elif c == ">":
+            in_tag = False
+        elif not in_tag:
+            print(c, end="")
+
+
+if __name__ == "__main__":
+    import sys
+
+    url = sys.argv[1] if len(sys.argv) > 1 else None
+    if url:
+        load(URL(url))
+    else:
+        print("Usage: python main.py <url>")

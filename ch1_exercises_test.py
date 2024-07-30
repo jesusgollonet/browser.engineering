145a711ef6dd80444185577bf51407330bc10bfd
Jesus Gollonet <jesusgollonet@gmail.com>
2024-07-30 17:46:35 +0200

    Start ch1 exercises test

---
+++ ch1_exercises_test.py
@@ -0,0 +1,10 @@
+"""
+1-1 HTTP/1.1. Along with Host, send the Connection header in the request function 
+with the value close. Your browser can now declare that it is using HTTP/1.1. 
+Also add a User-Agent header. Its value can be whatever you want—it identifies 
+your browser to the host. Make it easy to add further headers in the future.
+"""
+
+
+def test_ch1_ex11_():
+    return None

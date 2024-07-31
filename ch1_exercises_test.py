a4c4b1ce6e2b678532f7ad6df018101bf20034be
Jesus Gollonet <jesusgollonet@gmail.com>
2024-07-31 16:19:13 +0200

    Ex 1.2 file test

--- ch1_exercises_test.py
+++ ch1_exercises_test.py
@@ -1,14 +1,15 @@
+from browser.url import URL, RequestHeaders, Request, Net
+
 """
-1-1 HTTP/1.1. Along with Host, send the Connection header in the request function 
-with the value close. Your browser can now declare that it is using HTTP/1.1. 
-Also add a User-Agent header. Its value can be whatever you want—it identifies 
-your browser to the host. Make it easy to add further headers in the future.
+1-1 HTTP/1.1. Along with Host, send the Connection header in the request
+function with the value close. Your browser can now declare that it is using
+HTTP/1.1.  Also add a User-Agent header. Its value can be whatever you want—it
+identifies your browser to the host. Make it easy to add further headers in the
+future.
 """
 
-from browser.url import URL, RequestHeaders, Request
 
-
-def test_ch1_ex11():
+def test_ch1_ex11_http11():
     url = URL.parse("http://example.com")
     headers = RequestHeaders(
         headers={
@@ -27,4 +28,27 @@ def test_ch1_ex11():
     headers.add("Accept", "text/html")
     request_str = request.to_string()
     assert "Accept: text/html" in request_str
-    return None
+
+
+""" 
+1-2 File URLs. Add support for the file scheme, which allows the browser to
+open local files. For example, file:///path/goes/here should refer to the file
+on your computer at location /path/goes/here. Also make it so that, if your
+browser is started without a URL being given, some specific file on your
+computer is opened. You can use that file for quick testing 
+"""
+
+
+def test_ch1_ex12_file_urls():
+    url = URL.parse("file:///path/goes/here")
+    # url parsing
+    assert url.scheme == "file"
+    assert url.path == "/path/goes/here"
+    # file request
+    url = URL.parse(
+        "file:///Users/jgb/Learn/recurse.com/browser.engineering/mock/dummy.txt"
+    )
+    net = Net(url)
+    response = net.request(None)
+
+    assert "Raw content" in response
